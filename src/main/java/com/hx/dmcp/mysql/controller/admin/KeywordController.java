7dd84a5169d3933c050259500c7a8685a67b2324
krisjin <krisibm@163.com>
2014-11-25 07:42:35 +0800

    add controller

---
+++ src/main/java/com/hx/dmcp/mysql/controller/admin/KeywordController.java
@@ -0,0 +1,13 @@
+package com.hx.dmcp.mysql.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author krisjin
+ */
+@Controller
+@RequestMapping("/admin/ms")
+public class KeywordController {
+
+}
