303f29a456c3a0ce8a7b1e43ba29f08cba701fb5
krisjin <krisibm@163.com>
2014-11-25 17:37:59 +0800

    keyword controller

--- sql/dmcp.sql
+++ sql/dmcp.sql
@@ -173,6 +173,16 @@ AUTO_INCREMENT=1
 
 ;
 
+DROP TABLE IF EXISTS `keyword`;
+CREATE TABLE `keyword` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `value` text,
+  `stauts` int(11) DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- ----------------------------
 -- Auto increment value for `funcgroup_func`
 -- ----------------------------
--- src/main/java/com/hx/dmcp/mysql/controller/admin/KeywordController.java
+++ src/main/java/com/hx/dmcp/mysql/controller/admin/KeywordController.java
@@ -1,7 +1,16 @@
 package com.hx.dmcp.mysql.controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.hx.dmcp.mysql.entity.Keyword;
+import com.hx.dmcp.mysql.service.KeywordService;
+import com.hx.dmcp.util.Pagination;
 
 /**
  * @author krisjin
@@ -9,5 +18,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin/ms")
 public class KeywordController {
+	
+	@Autowired
+	private KeywordService keywordService;
 
+	@RequestMapping(value = "/keyword.htm", method = RequestMethod.GET)
+	public String listNews(@RequestParam(value = "p", defaultValue = "1") int p, ModelMap model) {
+		Pagination<Keyword> page = keywordService.getKeywordWithPage(p, 15);
+		model.put("page", page);
+		return "page/keyword/listKeyword";
+	}
+
+	@RequestMapping(value = "/keyword/{id}.htm", method = RequestMethod.GET)
+	public String keywordDetail(@PathVariable("id") int id, ModelMap model) {
+		Keyword keyword = keywordService.getKeywordById(id);
+		model.put("keyword", keyword);
+		return "page/keyword/keywordDetail";
+	}
+	
+	@RequestMapping(value = "/keyword/update.html", method = RequestMethod.GET)
+	public String updateKeyword(@RequestParam(value = "id", defaultValue = "0") int id, ModelMap model){
+		Keyword keyword =keywordService.getKeywordById(id);
+		model.put("keyword", keyword);
+		return "page/keyword/keywordDetail";
+	}
+	
 }
